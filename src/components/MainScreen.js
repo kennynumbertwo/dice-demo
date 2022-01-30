1927cc5a2ad30d89d0096e4f1c9482a1db5b4b2e
Kenny Tye <ktye1217@gmail.com>
2022-01-30 00:06:10 -0800

    fixed build errors

--- src/components/MainScreen.js
+++ src/components/MainScreen.js
@@ -1,17 +1,11 @@
-import React, { useState } from 'react';
-import DiceContainer from './DiceContainer';
+import React from 'react';
 import DiceContainerMobile from './DiceContainerMobile';
 import '../styles/MainScreen.scss';
 
 function MainScreen() {
-  const [isMobile, setIsMobile] = useState(true);
   return (
     <div className='MainScreenContainer'>
-      {isMobile ? (
-        <DiceContainerMobile />
-      ) : (
-        <DiceContainer />
-      )}
+      <DiceContainerMobile />
     </div>
   )
 }
